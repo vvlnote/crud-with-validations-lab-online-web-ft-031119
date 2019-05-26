8674abd400b3b0324c0e7b973ac021e75ed33529
Joy Lin <vvlnote@hotmail.com>
2019-05-26 22:43:42 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,56 @@
 class SongsController < ApplicationController
+  
+  def index
+    @songs = Song.all
+  end
+  
+  def new
+    @song = Song.new
+  end
+  
+  def create
+    binding.pry
+    @song = Song.create(song_params)
+    binding.pry
+    if @song.valid? 
+      redirect_to song_path(@song)
+    else
+      render :new
+    end
+  end
+  
+  def show
+    @song = find_song!
+  end
+  
+  def edit
+    @song = find_song!
+  end
+  
+  def update
+    @song = find_song!
+    if @song.valid? 
+      if @song.update(song_params) && @song.save
+        redirect_to song_path(@song)
+      else
+        render :edit
+      end
+    end
+  end
+  
+  def destroy
+    @song=find_song!
+    @song.destroy
+    redirect_to songs_path
+  end
+  
+  private
+  
+  def song_params
+    params.require(:song).permit(:title, :released, :release_year, :artist_name, :gener)
+  end
+  
+  def find_song!
+    @song = Song.find(params[:id])
+  end
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,2 +1,10 @@
 class Song < ApplicationRecord
+  validates :title, presence: true, uniqueness: true
+  validates :artist_name, presence: true
+  validates :released, inclusion: {in:[true, false]}
+  validates :release_year, presence: true,  if: :released_already?, numericality: {less_than_or_equal_to: Time.now.year}
+  
+  def released_already?
+    released == true
+  end
 end
